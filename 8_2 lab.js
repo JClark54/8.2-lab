75f5608cb11dd17dabce8dc2a8015da9c6029e29
JClark54 <85198081+JClark54@users.noreply.github.com>
2021-06-20 16:46:30 -0500

    Add files via upload

---
+++ 8_2 lab.js
@@ -0,0 +1,19 @@
+8_2 lab.js;
+'use strict';
+let firstnumber =5;
+let firstString = 'Hello JS';
+let secondnumber = 10;
+console.log=(firstnumber+secondnumber);
+firstnumber=50
+console.log(firstnumber+secondnumber)
+let secondstring = "this is text";
+console.log(firstString + secondstring);
+let novalueassigned;
+console.log(novalueassigned);
+let nothingvalue= null;
+console.log(nothingvalue);
+let true_or_false=true;
+const nothingcanchangeme= "this value cannot be changed";
+
+
+
